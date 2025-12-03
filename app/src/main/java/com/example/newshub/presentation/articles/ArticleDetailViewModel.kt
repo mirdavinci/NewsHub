fc0f213fe29aa774fa9411da9e16893c4295dcb7
mirdar <m.mirdar@farazware.com>
2025-12-03 15:29:21 +0330

    implement viewmodel

--- app/src/main/java/com/example/newshub/presentation/articles/ArticleDetailViewModel.kt
+++ app/src/main/java/com/example/newshub/presentation/articles/ArticleDetailViewModel.kt
@@ -1,9 +1,57 @@
 package com.example.newshub.presentation.articles
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.newshub.domain.model.ArticleModel
+import com.example.newshub.domain.usecase.GetArticleDetailUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-data class ArticleDetailViewModel(
+data class ArticleDetailUiState(
     val isLoading: Boolean = false,
     val article: ArticleModel? = null,
     val error: String? = null
 )
+
+@HiltViewModel
+class ArticleDetailViewModel @Inject constructor(
+
+    private val getArticleDetailUseCase: GetArticleDetailUseCase
+) : ViewModel() {
+
+    private val _uiState = mutableStateOf(ArticleDetailUiState())
+    val uiState: State<ArticleDetailUiState> = _uiState
+
+    fun loadArticle(id: String) {
+
+        _uiState.value = _uiState.value.copy(isLoading = true, error = null)
+        viewModelScope.launch {
+            try {
+                val result = getArticleDetailUseCase(id)
+                if (result != null) {
+
+                    _uiState.value = _uiState.value.copy(
+                        isLoading = false,
+                        article = result
+                    )
+                } else {
+                    _uiState.value = _uiState.value.copy(
+                        isLoading = false,
+                        error = "Article not found"
+                    )
+                }
+            } catch (e: Exception) {
+                _uiState.value = _uiState.value.copy(
+                    isLoading = false,
+                    error = e.message ?: "Unknown error"
+                )
+            }
+        }
+
+    }
+
+
+}
